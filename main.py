1273962b5ce9aa8d1b954e4ca493368d4b7d7c52
MustafaTheCoder <73116655+MustafaTheCoder@users.noreply.github.com>
2021-08-20 11:06:29 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+import pyautogui
+import string
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+chars_list = list(chars)
+
+password = pyautogui.password("Enter Password: ")
+guess_password = ""
+
+while(guess_password != password):
+	guess_password = random.choices(chars_list, k=len(password))
+	print("<============"+ str(guess_password) +"============>")
+
+	if(guess_password == list(password)):
+		print("Cracked Password: " + "".join(guess_password))
+		input()
+		break
+ 
